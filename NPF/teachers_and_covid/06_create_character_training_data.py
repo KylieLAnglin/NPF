3bc64fce2728e0415ea182a729dd4bf84986eff6
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-02-22 07:28:01 -0500

    create character training data

--- NPF/teachers_and_covid/06_create_character_training_data.py
+++ NPF/teachers_and_covid/06_create_character_training_data.py
@@ -4,17 +4,30 @@ from NPF.teachers_and_covid import start
 from NPF.library import classify
 
 # %%
-tweets = pd.read_csv(start.MAIN_DIR + "tweets_classified.csv")
+tweet_classifications = pd.read_csv(start.MAIN_DIR + "tweets_classified.csv")
+tweet_classifications["unique_id"] = pd.to_numeric(
+    tweet_classifications.unique_id, errors="coerce"
+)
+tweets = pd.read_csv(start.MAIN_DIR + "tweets_full.csv")
+
+tweets = tweets.merge(
+    tweet_classifications[["unique_id", "classification_rule"]],
+    left_on="unique_id",
+    right_on="unique_id",
+    how="left",
+    indicator=True,
+)
+
 tweets = tweets[tweets.classification_rule == 1]
 
+# %%
+training_batch_5 = tweets[tweets.random_set == 5]
+training_batch_5.to_csv(start.MAIN_DIR + "training_batch5.csv")  # joe
+
+training_batch_6 = tweets[tweets.random_set == 6]
+training_batch_6.to_csv(start.MAIN_DIR + "training_batch6.csv")  # jessica
+
+training_batch_7 = tweets[tweets.random_set == 7]
+training_batch_7.to_csv(start.MAIN_DIR + "training_batch7.csv")  # kylie
 
 # %%
-training_batch_5 = tweets[tweets.random_set == 5].to_csv(
-    start.MAIN_DIR + "training_batch5.csv"
-)
-training_batch_6 = tweets[tweets.random_set == 6].to_csv(
-    start.MAIN_DIR + "training_batch6.csv"
-)
-training_batch_7 = tweets[tweets.random_set == 7].to_csv(
-    start.MAIN_DIR + "training_batch7.csv"
-)
