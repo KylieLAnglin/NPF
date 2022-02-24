c73be71462e75931640d144816ed8289223bf708
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-02-24 07:02:00 -0500

    separate import and export annotations

--- NPF/teachers_and_covid/02_character_classifier/01_import_character_annotations.py
+++ NPF/teachers_and_covid/02_character_classifier/01_import_character_annotations.py
@@ -54,6 +54,7 @@ df = pd.concat(
 
 df = df.merge(tweets, left_on=["unique_id"], right_on=["unique_id"])
 
+
 # %%
 df = df[df.category.isin([1, 2, 3, 4])]
 df = df[(df.relevant != 0) | (df.category == 1)]
@@ -66,69 +67,6 @@ df["villain"] = np.where(df.category == 2, 1, 0)
 df["victim"] = np.where(df.category == 3, 1, 0)
 df["other"] = np.where(df.category == 4, 1, 0)
 
-
-df["text"] = [re.sub(r"[^\w\s]", "", s) for s in df.text]  # remove punctuation
-df["text"] = df.text.str.replace("\n", " ", regex=False)  # remove new line
-df["text"] = df.text.str.replace("\xa0", " ", regex=False)  # remove utf errors
-
-# %%
-training = df[df.random_set != 3]
-testing = df[df.random_set == 3]
-
-# %%
-train_cats = [
-    {"HERO": hero, "VILLAIN": villain, "VICTIM": victim, "OTHER": other}
-    for hero, villain, victim, other in zip(
-        training.hero, training.villain, training.victim, training.other
-    )
-]
-
-
-train_list = []
-for text, cat in zip(training.text, train_cats):
-    train_list.append({"text": text, "cats": cat})
-train_list = [str(item) for item in train_list]
-
-with open(
-    "/Users/kla21002/textcat_tweets_characters/assets/json_training.jsonl", "w"
-) as f:
-    for line in train_list:
-        f.write(line.replace("'", '"'))
-        f.write("\n")
-# %%
-test_cats = [
-    {"HERO": hero, "VILLAIN": villain, "VICTIM": victim, "OTHER": other}
-    for hero, villain, victim, other in zip(
-        testing.hero, testing.villain, testing.victim, testing.other
-    )
-]
-
-
-test_list = []
-for text, cat in zip(testing.text, test_cats):
-    test_list.append({"text": text, "cats": cat})
-
-test_list = [str(item) for item in test_list]
-
-with open(
-    "/Users/kla21002/textcat_tweets_characters/assets/json_testing.jsonl", "w"
-) as f:
-    for line in test_list:
-        f.write(line.replace("'", '"'))
-        f.write("\n")
-
-# %%
-df[
-    [
-        "unique_id",
-        "category",
-        "relevant",
-        "irrelevant",
-        "hero",
-        "villain",
-        "victim",
-        "other",
-    ]
-].to_csv(start.MAIN_DIR + "annotations_characters.csv")
-
+df.to_csv(start.MAIN_DIR + "annotations_characters.csv", encoding="utf-8")
+df.to_pickle(start.MAIN_DIR + "annotations_characters.pkl")
 # %%
---
+++ NPF/teachers_and_covid/02_character_classifier/01b_export_character_annotations.py
@@ -0,0 +1,67 @@
+# %%
+
+import pandas as pd
+import numpy as np
+import re
+import pickle
+
+from NPF.teachers_and_covid import start
+
+
+# %%
+df = pd.read_pickle(start.MAIN_DIR + "annotations_characters.pkl")
+
+# %%
+df["text"] = [re.sub(r"[^\w\s]", "", s) for s in df.text]  # remove punctuation
+df["text"] = df.text.str.replace("\n", " ", regex=False)  # remove new line
+df["text"] = df.text.str.replace("\xa0", " ", regex=False)  # remove utf errors
+
+# %%
+training = df[df.random_set != 3]
+testing = df[df.random_set == 3]
+
+# %%
+train_cats = [
+    {"HERO": hero, "VILLAIN": villain, "VICTIM": victim, "OTHER": other}
+    for hero, villain, victim, other in zip(
+        training.hero, training.villain, training.victim, training.other
+    )
+]
+
+
+train_list = []
+for text, cat in zip(training.text, train_cats):
+    train_list.append({"text": text, "cats": cat})
+train_list = [str(item) for item in train_list]
+
+with open(
+    "/Users/kla21002/textcat_tweets_characters/assets/json_training.jsonl", "w"
+) as f:
+    for line in train_list:
+        f.write(line.replace("'", '"'))
+        f.write("\n")
+# %%
+test_cats = [
+    {"HERO": hero, "VILLAIN": villain, "VICTIM": victim, "OTHER": other}
+    for hero, villain, victim, other in zip(
+        testing.hero, testing.villain, testing.victim, testing.other
+    )
+]
+
+
+test_list = []
+for text, cat in zip(testing.text, test_cats):
+    test_list.append({"text": text, "cats": cat})
+
+test_list = [str(item) for item in test_list]
+
+with open(
+    "/Users/kla21002/textcat_tweets_characters/assets/json_testing.jsonl", "w"
+) as f:
+    for line in test_list:
+        f.write(line.replace("'", '"'))
+        f.write("\n")
+
+# %%
+
+# %%
