6d7cdc6628cef16d5443beaf8a27246bf998090e
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-03-02 13:31:24 -0500

    time trends full and save spacy results

---
+++ NPF/teachers_and_covid/02_character_classifier/05_spacy_model.py
@@ -0,0 +1,101 @@
+# %%
+import pandas as pd
+import os
+import spacy
+from spacy.util import minibatch, compounding
+import numpy as np
+import re
+
+CLEAN_DIR = "/Volumes/GoogleDrive/.shortcut-targets-by-id/1wL0tiiBznVSn5Gij9hYTiC6HTPCExAn8/Policy Narratives and NLP/Data and Code/"
+MODEL_DIR = "/Users/kla21002/textcat_tweets_characters/packages/en_textcat_main-0.0.0/en_textcat_demo/en_textcat_demo-0.0.0"
+TEMP_DIR = "/Users/kla21002/Dropbox/Active/NPF/data/"
+# %%
+tweets = pd.read_csv(CLEAN_DIR + "tweets_full.csv")
+tweets = tweets[
+    [
+        "unique_id",
+        "text",
+        "created",
+        "likes",
+        "retweets",
+        "quotes",
+        "replies",
+        "author_id",
+        "geo",
+        "random_set",
+    ]
+]
+tweets["text"] = [re.sub(r"[^\w\s]", "", s) for s in tweets.text]  # remove punctuation
+tweets["text"] = tweets.text.str.replace("\n", " ", regex=False)  # remove new line
+tweets["text"] = tweets.text.str.replace("\xa0", " ", regex=False)  # remove utf errors
+
+annotations = pd.read_csv(CLEAN_DIR + "annotations_characters.csv")
+annotations = annotations[
+    [
+        "unique_id",
+        "category",
+    ]
+]
+
+df = tweets.merge(annotations, how="left", left_on="unique_id", right_on="unique_id")
+
+
+training = df[df.random_set != 3]
+testing = df[df.random_set == 3]
+
+# %%
+# apply the saved model
+print("Loading from", MODEL_DIR)
+nlp = spacy.load(MODEL_DIR)
+categories = []
+for text in testing.text:
+    doc = nlp(text)
+    categories.append(doc.cats)
+
+testing["spacy_hero"] = [label["HERO"] for label in categories]
+testing["spacy_villain"] = [label["VILLAIN"] for label in categories]
+testing["spacy_victim"] = [label["VICTIM"] for label in categories]
+testing["spacy_other"] = [label["OTHER"] for label in categories]
+
+testing.to_csv(TEMP_DIR + "testing_characters_spacy.csv")
+# %%
+
+# apply the saved model
+print("Loading from", MODEL_DIR)
+nlp = spacy.load(MODEL_DIR)
+categories = []
+for text in training.text:
+    doc = nlp(text)
+    categories.append(doc.cats)
+
+training["spacy_hero"] = [label["HERO"] for label in categories]
+training["spacy_villain"] = [label["VILLAIN"] for label in categories]
+training["spacy_victim"] = [label["VICTIM"] for label in categories]
+training["spacy_other"] = [label["OTHER"] for label in categories]
+training.to_csv(TEMP_DIR + "training_characters_spacy.csv")
+
+
+# %%
+
+# apply the saved model
+print("Loading from", MODEL_DIR)
+nlp = spacy.load(MODEL_DIR)
+categories = []
+for text in tweets.text:
+    doc = nlp(text)
+    categories.append(doc.cats)
+
+tweets["spacy_hero"] = [label["HERO"] for label in categories]
+tweets["spacy_villain"] = [label["VILLAIN"] for label in categories]
+tweets["spacy_victim"] = [label["VICTIM"] for label in categories]
+tweets["spacy_other"] = [label["OTHER"] for label in categories]
+
+tweets[
+    [
+        "unique_id",
+        "spacy_hero",
+        "spacy_villain",
+        "spacy_victim",
+        "spacy_other",
+    ]
+].to_csv(TEMP_DIR + "model_characters_spacy.csv", index=False)
---
+++ NPF/teachers_and_covid/03_analysis/time_trends_full.py
@@ -0,0 +1,96 @@
+# %%
+import pandas as pd
+import numpy as np
+
+from NPF.teachers_and_covid import start
+import matplotlib.pyplot as plt
+
+classifications = pd.read_csv(start.TEMP_DIR + "model_characters_spacy.csv")
+
+tweets = pd.read_csv(start.CLEAN_DIR + "tweets_full.csv")
+
+df = tweets.merge(
+    classifications,
+    left_on="unique_id",
+    right_on="unique_id",
+    how="left",
+    indicator=True,
+)
+
+df.to_csv(start.CLEAN_DIR + "tweets_final.csv")
+# %%
+df["hero"] = np.where(df.spacy_hero > 0.5, 1, 0)
+df["villain"] = np.where(df.spacy_villain > 0.5, 1, 0)
+df["victim"] = np.where(df.spacy_victim > 0.5, 1, 0)
+
+df["date"] = pd.to_datetime(df.created, errors="coerce")
+df["month"] = df["date"].dt.month
+
+melted = df[["month", "hero", "villain", "victim"]].groupby("month").agg("sum")
+# %%
+df = df.set_index("date")
+monthly = df[["hero", "villain", "victim"]].resample("M").sum()
+
+# %%
+plt.plot(monthly.index, monthly.hero, label="Hero")
+plt.plot(monthly.index, monthly.villain, label="Villain")
+plt.plot(monthly.index, monthly.victim, label="Victim")
+plt.legend()
+# %%
+
+# %%
+daily = df[["hero", "villain", "victim"]].resample("D").sum()
+
+plt.plot(daily.index, daily.hero, label="Hero")
+plt.plot(daily.index, daily.villain, label="Villain")
+plt.plot(daily.index, daily.victim, label="Victim")
+plt.xticks(
+    [
+        "2020-03-01 00:00:00+00:00",
+        "2020-04-01 00:00:00+00:00",
+        "2020-05-01 00:00:00+00:00",
+        "2020-06-01 00:00:00+00:00",
+        "2020-07-01 00:00:00+00:00",
+        "2020-08-01 00:00:00+00:00",
+        "2020-09-01 00:00:00+00:00",
+        "2020-10-01 00:00:00+00:00",
+        "2020-11-01 00:00:00+00:00",
+        "2020-12-01 00:00:00+00:00",
+        "2021-01-01 00:00:00+00:00",
+        "2021-02-01 00:00:00+00:00",
+    ],
+    [
+        "3-20",
+        "4-20",
+        "5-20",
+        "6-20",
+        "7-20",
+        "8-20",
+        "9-20",
+        "10-20",
+        "11-20",
+        "12-20",
+        "1-21",
+        "2-21",
+    ],
+)
+plt.legend()
+plt.xlabel("Date")
+plt.ylabel("Number of Tweets")
+
+# %%
+
+df["hero_weighted"] = df.hero * df.likes
+df["villain_weighted"] = df.villain * df.likes
+df["victim_weighted"] = df.victim * df.likes
+
+daily_weighted = (
+    df[["hero_weighted", "villain_weighted", "victim_weighted"]].resample("D").sum()
+)
+
+plt.plot(daily_weighted.index, daily_weighted.hero, label_weighted="Hero")
+plt.plot(daily_weighted.index, daily_weighted.villain, label="Villain")
+plt.plot(daily_weighted.index, daily_weighted.victim, label="Victim")
+plt.legend()
+
+# %%
