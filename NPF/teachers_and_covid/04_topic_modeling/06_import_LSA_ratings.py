8b10bbe1a81fd2886a0e94d1e759b34f4df57638
KylieLAnglin <kylie.anglin@uconn.edu>
2023-06-20 20:18:22 -0500

    import lsa ratings from both coders

--- NPF/teachers_and_covid/04_topic_modeling/06_import_LSA_ratings.py
+++ NPF/teachers_and_covid/04_topic_modeling/06_import_LSA_ratings.py
@@ -7,23 +7,50 @@ import string
 from NPF.teachers_and_covid import start
 
 # %%
-df = pd.read_excel(start.ANNOTATIONS_DIR + "word_coherence_ratings_lsa_TF.xlsx")
-df = df.head(7)
-df
+df_tf = pd.read_excel(start.ANNOTATIONS_DIR + "word_coherence_ratings_lsa_TF.xlsx")
+df_tf = df_tf.head(7)
+df_tf
 # %%
-df_long = df.T
-df_long.columns = df_long.iloc[0]
-df_long = df_long.drop(df_long.index[0])
-df_long = df_long.rename(
-    columns={"Coherence Rating": "coherence", "Specificity Rating": "specificity"}
+df_zm = pd.read_excel(start.ANNOTATIONS_DIR + "word_coherence_ratings_lsa_ZM.xlsx")
+df_zm = df_zm.head(7)
+df_zm
+# %%
+df_tf_long = df_tf.T
+df_tf_long.columns = df_tf_long.iloc[0]
+df_tf_long = df_tf_long.drop(df_tf_long.index[0])
+df_tf_long = df_tf_long.rename(
+    columns={"Coherence Rating": "coherence_tf", "Specificity Rating": "specificity_tf"}
 )
-df_long["score"] = df_long.coherence * df_long.specificity
+df_tf_long["score_tf"] = df_tf_long.coherence_tf * df_tf_long.specificity_tf
+df_tf_long
+# %%
+df_zm_long = df_zm.T
+df_zm_long.columns = df_zm_long.iloc[0]
+df_zm_long = df_zm_long.drop(df_zm_long.index[0])
+df_zm_long = df_zm_long.rename(
+    columns={"Coherence Rating": "coherence_zm", "Specificity Rating": "specificity_zm"}
+)
+df_zm_long["score_zm"] = df_zm_long.coherence_zm * df_zm_long.specificity_zm
+
+df_zm_long
+
+# %%
+df_long = df_tf_long.merge(
+    df_zm_long[["coherence_zm", "specificity_zm", "score_zm"]],
+    left_index=True,
+    right_index=True,
+)
+df_long = df_long.sort_index()
 df_long
 # %%
 df_long = df_long.reset_index()
 df_long = df_long.rename(columns={"index": "source"})
 df_long[["model", "topic"]] = df_long["source"].str.rsplit(pat="_", n=1, expand=True)
 df_long
+# %%
+df_long["coherence"] = (df_long.coherence_zm + df_long.coherence_tf) / 2
+df_long["specificity"] = (df_long.specificity_zm + df_long.specificity_tf) / 2
+df_long["score"] = (df_long.score_zm + df_long.score_tf) / 2
 # %%
 df_topics = df_long[
     [
@@ -32,21 +59,25 @@ df_topics = df_long[
         "score",
         "coherence",
         "specificity",
+        "score_tf",
+        "coherence_tf",
+        "specificity_tf",
+        "score_zm",
+        "coherence_zm",
+        "specificity_zm",
     ]
 ]
 df_topics
 df_topics.sort_values(by=["model", "topic"])
-df_topics = df_topics[df_topics.topic < "20"]
 # %%
 df_best_topics = df_topics[["model", "score"]].groupby("model").head(5)
 df_models = df_best_topics[["model", "score"]].groupby("model").mean()
 df_models = df_models.sort_values(by="score", ascending=False)
 df_models
 # %%
-MODEL = "lsa_no_below_500_no_above_1.0"
+MODEL = "lsa_no_below_100_no_above_0.5"
 df_topics[df_topics.model == MODEL].sort_values(by="score", ascending=False)
-
-TOPIC = "17"
+TOPIC = "2"
 
 tweets = pd.read_csv(start.RESULTS_DIR + "LSA/" + MODEL + "/lsa_tweets.csv")
 tweets = tweets.sort_values(by=TOPIC, ascending=False)
