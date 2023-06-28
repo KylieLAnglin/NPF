c60eac49869db099fbbaf560305d3f3f37520ecb
KylieLAnglin <kylie.anglin@uconn.edu>
2023-06-28 10:11:54 -0400

    create interpretation files

--- NPF/teachers_and_covid/04_topic_modeling/05_import_LDA_ratings.py
+++ NPF/teachers_and_covid/04_topic_modeling/05_import_LDA_ratings.py
@@ -83,31 +83,79 @@ df_topics
 df_topics.sort_values(by=["model", "topic"])
 df_topics.to_csv(start.CLEAN_DIR + "model_topic_ratings.csv")
 # %%
-METRIC = "score"
-# METRIC = "coherence"
 
-df_best_topics = df_topics[["model", METRIC]].groupby("model").head(5)
-df_models = df_best_topics[["model", METRIC]].groupby("model").mean()
-df_models = df_models.sort_values(by=METRIC, ascending=False)
-df_models
+df_best_topics_score = df_topics[["model", "score"]].groupby("model").head(5)
+df_models_score = df_best_topics_score[["model", "score"]].groupby("model").mean()
+
+df_best_topics_coherence = df_topics[["model", "coherence"]].groupby("model").head(5)
+df_models_coherence = (
+    df_best_topics_coherence[["model", "coherence"]].groupby("model").mean()
+)
+
+df_best_topics_specificity = (
+    df_topics[["model", "specificity"]].groupby("model").head(5)
+)
+df_models_specificity = (
+    df_best_topics_specificity[["model", "specificity"]].groupby("model").mean()
+)
+
+df_models = df_models_coherence.merge(
+    df_models_specificity, left_index=True, right_index=True
+)
+df_models = df_models.merge(df_models_score, left_index=True, right_index=True)
+
+
 # %%
-MODEL = "topic_10_no_below_50_no_above_1"
-TOPIC = "7"
-df_topics[df_topics.model == MODEL].sort_values(by="score", ascending=False)
 
+MODEL = "topic_20_no_below_100_no_above_1"  # top score, top coherence
+
+# %%
+
+words = pd.read_csv(start.RESULTS_DIR + "topic_models/" + MODEL + "/topics.csv")
+words
+
+words.to_excel(start.ANNOTATIONS_DIR + "topic_interpretation/words_" + MODEL + ".xlsx")
+
+# %%
 tweets = pd.read_csv(start.RESULTS_DIR + "topic_models/" + MODEL + "/tweet_topics.csv")
-tweets = tweets.sort_values(by=TOPIC, ascending=False)
-list(tweets.head(10).text)
+tweets = tweets.drop_duplicates(subset="text", keep="first")
+
+
+topics = [col for col in words.columns if "Word" in col]
+
+top_tweets_lists = []
+for topic in topics:
+    topic = topic.replace("Word_", "")
+    tweets = tweets.sort_values(by=topic, ascending=False)
+    top_tweets_lists.append(list(tweets.head(250).text))
+
+tweets_df = pd.DataFrame(top_tweets_lists).T
+tweets_df.to_excel(
+    start.ANNOTATIONS_DIR + "topic_interpretation/tweets_" + MODEL + ".xlsx"
+)
+# %%
+MODEL = "topic_10_no_below_50_no_above_1"  # top score
+
 
 words = pd.read_csv(start.RESULTS_DIR + "topic_models/" + MODEL + "/topics.csv")
-# %%
-# MODEL = "topic_20_no_below_100_no_above_1"
-# df_topics[df_topics.model == MODEL].sort_values(by="score", ascending=False)
+words
 
-# TOPIC = "15"
+words.to_excel(start.ANNOTATIONS_DIR + "topic_interpretation/words_" + MODEL + ".xlsx")
 
-# tweets = pd.read_csv(start.RESULTS_DIR + "topic_models/" + MODEL + "/tweet_topics.csv")
-# tweets = tweets.sort_values(by=TOPIC, ascending=False)
-# list(tweets.head(5).text)
 # %%
-words = pd.read_csv(start.RESULTS_DIR + "topic_models/" + MODEL + "/topics.csv")
+tweets = pd.read_csv(start.RESULTS_DIR + "topic_models/" + MODEL + "/tweet_topics.csv")
+tweets = tweets.drop_duplicates(subset="text", keep="first")
+
+
+topics = [col for col in words.columns if "Word" in col]
+
+top_tweets_lists = []
+for topic in topics:
+    topic = topic.replace("Word_", "")
+    tweets = tweets.sort_values(by=topic, ascending=False)
+    top_tweets_lists.append(list(tweets.head(250).text))
+
+tweets_df = pd.DataFrame(top_tweets_lists).T
+tweets_df.to_excel(
+    start.ANNOTATIONS_DIR + "topic_interpretation/tweets_" + MODEL + ".xlsx"
+)
--- NPF/teachers_and_covid/04_topic_modeling/06_import_LSA_ratings.py
+++ NPF/teachers_and_covid/04_topic_modeling/06_import_LSA_ratings.py
@@ -69,22 +69,55 @@ df_topics = df_long[
 ]
 df_topics
 df_topics.sort_values(by=["model", "topic"])
-# %%
-df_best_topics = df_topics[["model", "score"]].groupby("model").head(5)
-df_models = df_best_topics[["model", "score"]].groupby("model").mean()
-df_models = df_models.sort_values(by="score", ascending=False)
-df_models
-# %%
-MODEL = "lsa_no_below_100_no_above_0.5"
-df_topics[df_topics.model == MODEL].sort_values(by="score", ascending=False)
-TOPIC = "2"
 
-tweets = pd.read_csv(start.RESULTS_DIR + "LSA/" + MODEL + "/lsa_tweets.csv")
-tweets = tweets.sort_values(by=TOPIC, ascending=False)
-list(tweets.head(5).tweet_text)
+# %%
+df_best_topics_score = df_topics[["model", "score"]].groupby("model").head(5)
+df_models_score = df_best_topics_score[["model", "score"]].groupby("model").mean()
+
+df_best_topics_coherence = df_topics[["model", "coherence"]].groupby("model").head(5)
+df_models_coherence = (
+    df_best_topics_coherence[["model", "coherence"]].groupby("model").mean()
+)
+
+df_best_topics_specificity = (
+    df_topics[["model", "specificity"]].groupby("model").head(5)
+)
+df_models_specificity = (
+    df_best_topics_specificity[["model", "specificity"]].groupby("model").mean()
+)
+
+df_models = df_models_coherence.merge(
+    df_models_specificity, left_index=True, right_index=True
+)
+df_models = df_models.merge(df_models_score, left_index=True, right_index=True)
+df_models
+
+# %%
+
+MODEL = "lsa_no_below_10_no_above_1.0"  # top score, top coherence
 
 # %%
 words = pd.read_csv(start.RESULTS_DIR + "LSA/" + MODEL + "/lsa_words.csv")
-words = words.sort_values(by=TOPIC, ascending=False)
-words.head()
+words
+
+words.to_excel(start.ANNOTATIONS_DIR + "topic_interpretation/words_" + MODEL + ".xlsx")
+
+# %%
+tweets = pd.read_csv(start.RESULTS_DIR + "LSA/" + MODEL + "/lsa_tweets.csv")
+tweets = tweets.drop_duplicates(subset="tweet_text", keep="first")
+
+
+topics = [col for col in words.columns if "Word" in col]
+
+top_tweets_lists = []
+for topic in topics:
+    topic = topic.replace("Word_", "")
+    tweets = tweets.sort_values(by=topic, ascending=False)
+    top_tweets_lists.append(list(tweets.head(250).tweet_text))
+
+tweets_df = pd.DataFrame(top_tweets_lists).T
+tweets_df.to_excel(
+    start.ANNOTATIONS_DIR + "topic_interpretation/tweets_" + MODEL + ".xlsx"
+)
+
 # %%
